86bedc23abc97197d80d5fd1e42f556322a49b38
kendrick chukwuka <kendrickchibueze@gmail.com>
2024-02-28 12:49:17 +0100

    Add project files.

---
+++ DataConversionAndExceptionInCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DataConversionAndExceptionInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Conversion and Exception in C#...");
+
+            try
+            {
+                Rectangle rc = new Rectangle(8.9, 7.9);
+                Console.WriteLine("The Area before boxing is : {0}", rc.Area); 
+                int newArea = (int)rc.Area;
+                Console.WriteLine("The type conversion from double to int is : {0}", newArea);
+                object o = rc;
+                Console.WriteLine("We have boxed the area: {0}", o.GetType());     
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception : " +  ex.Message);   
+            }      
+        }
+    }
+}
---
+++ DataConversionAndExceptionInCSharp/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿namespace DataConversionAndExceptionInCSharp
+{
+    internal class Rectangle
+    {
+        private double _length;
+        private double _width;
+        public Rectangle(double length, double width)
+        {
+            SetLength(length);
+            SetWidth(width);
+        }
+        public double Length
+        {
+            get { return _length; }
+            set { SetLength(value); }
+        }
+        public double Width
+        {
+            get { return _width; }
+            set { SetWidth(value); }
+        }
+        private void SetLength(double value)
+        {
+            if (value >= 0)
+                _length = value;
+            else
+                throw new ArgumentException("Length must be greater than or equal to 0");
+        }
+        private void SetWidth(double value)
+        {
+            if (value >= 0)
+                _width = value;
+            else
+                throw new ArgumentException("Width must be greater than or equal to 0");
+        }
+        public double Area
+        {
+            get
+            {
+                return Length * Width;
+            }
+        }
+    }
+}
