27dd815659c18043be06af8c251f2dacf69eea70
kendrick chukwuka <kendrickchibueze@gmail.com>
2024-02-29 22:31:11 +0100

    feat:collecting inputs from users

---
+++ DataConversionAndExceptionInCSharp/MyRectangle.cs
@@ -0,0 +1,72 @@
+﻿namespace DataConversionAndExceptionInCSharp
+{
+    internal class MyRectangle
+    {
+        private double _width;
+        private double _length;
+
+        public MyRectangle()
+        {
+            
+        }
+        public MyRectangle(double width, double length)
+        {
+            SetLength(length);
+            SetWidth(width);
+            
+        }
+
+        public double Length { 
+            get 
+            {
+                return _length;
+            } 
+        }
+        public double  Width { 
+            get 
+            {
+                return _width;
+            }
+        }
+
+        public void SetLength(double value)
+        {
+            if(value > 0)
+            {
+               _length = value;
+            }
+            else
+            {
+                throw new ArgumentException("The length must not be less than or equal to 0");
+            }
+           
+
+        }
+
+        public void SetWidth(double value)
+        {
+            if(value >0)
+            {
+                _width = value;
+            }
+            else
+            {
+                throw new ArgumentException("The width must not be less than or equal to 0");
+            }
+        }
+
+      /*  public double Area {
+            get
+            {
+                return Length * Width;
+            }
+        }*/
+
+        public double Area()
+        {
+            return Length * Width;
+        }
+
+
+    }
+}
--- DataConversionAndExceptionInCSharp/Program.cs
+++ DataConversionAndExceptionInCSharp/Program.cs
@@ -5,8 +5,33 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Data Conversion and Exception in C#...");
+            Console.WriteLine();
 
             try
+            {
+                MyRectangle myrec = new MyRectangle();
+                Console.WriteLine("Enter the Length : ");
+                myrec.SetLength(Convert.ToDouble(Console.ReadLine()));
+                Console.WriteLine();
+                Console.WriteLine("Enter the width : ");
+                myrec.SetWidth(Convert.ToDouble(Console.ReadLine()));
+                Console.WriteLine("The Area of the rectangle without conversion is : {0}", myrec.Area());
+
+                int MyArea = (int)myrec.Area(); // converted from double to an int
+                Console.WriteLine("The Area after converting to Int is : {0}", MyArea);
+                Console.WriteLine("Boxing output");
+                object boxedOutput = MyArea;
+                Console.WriteLine("The is boxing return : {0}", boxedOutput);
+                int unboxedOutput = (int)boxedOutput;
+                Console.WriteLine("The unboxed value is : {0}", unboxedOutput);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+           /* try
             {
                 Rectangle rc = new Rectangle(8.9, 7.9);
                 Console.WriteLine("The Area before boxing is : {0}", rc.Area); 
@@ -18,7 +43,7 @@
             catch(Exception ex)
             {
                 Console.WriteLine("Exception : " +  ex.Message);   
-            }      
+            } */     
         }
     }
 }
